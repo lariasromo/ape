58d6837f9722a4658ea207f90758b3ec0f5d09f1
LuisArias <luis.arias@libertexgroup.com>
2023-03-30 20:26:26 +0100

    Added examples

--- src/test/scala/com/libertexgroup/ape/pipelines/PipelineTest.scala
+++ src/test/scala/com/libertexgroup/ape/pipelines/PipelineTest.scala
@@ -12,24 +12,23 @@ import zio.kafka.consumer.Consumer
 import zio.test.{Spec, TestEnvironment, ZIOSpec, assertTrue}
 import zio.{Chunk, Scope, ZIO, ZLayer}
 
-object PipelineTest extends ZIOSpec[ KafkaContainer with KafkaConfig with Consumer with MultiClickhouseConfig with ClickHouseContainer] {
-  implicit val t: ConsumerRecord[String, Array[Byte]] => ClickhouseModel = rec =>
-    dummy(rec.value().map(_.toChar).mkString, rec.value().map(_.toChar).mkString)
-
-  val pp: ZIO[KafkaConfig with Consumer with MultiClickhouseConfig, Throwable, Ape[Consumer, (ClickhouseModel, Chunk[ClickhouseModel])]] =
-    Ape.readers.kafkaDefaultReader.**[ClickhouseModel] --> (Ape.writers.consoleWriter[Consumer, ClickhouseModel] ++ Ape.writers.clickhouseWriter[Consumer])
+object PipelineTest extends ZIOSpec[KafkaContainer with KafkaConfig with Consumer with MultiClickhouseConfig with ClickHouseContainer] {
+  val pp = Ape.readers.kafkaDefaultReader.**[dummy] --> (
+      Ape.writers.consoleWriter[Any, Consumer, dummy] ++ Ape.writers.clickhouseWriter[Consumer, dummy]
+    )
     
-  override def spec: Spec[ KafkaContainer with KafkaConfig with Consumer with MultiClickhouseConfig with ClickHouseContainer with TestEnvironment with Scope, Any] = suite("PipelineTest")(
-    test("Simple pipeline with PipelineBuilder"){
-      for {
-        _ <- KafkaContainerService.sendPlaintextMessage
-        pipe <- pp
-        _ <- pipe.run
-      } yield {
-        assertTrue(true)
-      }
-    },
-  )
+  override def spec: Spec[KafkaContainer with KafkaConfig with Consumer with MultiClickhouseConfig with ClickHouseContainer with TestEnvironment with Scope, Any] =
+    suite("PipelineTest")(
+      test("Simple pipeline with PipelineBuilder"){
+        for {
+          _ <- KafkaContainerService.sendPlaintextMessage
+          pipe <- pp
+          _ <- pipe.stream.runHead
+        } yield {
+          assertTrue(true)
+        }
+      },
+    )
 
   override def bootstrap: ZLayer[Any, Throwable, KafkaContainer with KafkaConfig with Consumer with MultiClickhouseConfig with ClickHouseContainer] =
     KafkaContainerService.topicLayer("pipe_topic") >+> KafkaUtils.consumerLayer ++ ClickhouseContainerService.layer
