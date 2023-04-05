4c3b288d6da9edd022069cc2a255639b220715e5
LuisArias <luisarias@encompasscorporation.com>
2023-04-05 16:20:30 +0100

    CsvWriter

--- src/main/scala/com/libertexgroup/ape/writers/s3/CsvWriter.scala
+++ src/main/scala/com/libertexgroup/ape/writers/s3/CsvWriter.scala
@@ -1,27 +1,24 @@
 package com.libertexgroup.ape.writers.s3
 
 import com.libertexgroup.configs.S3Config
+import purecsv.safe._
+import purecsv.safe.converter.Converter
 import zio.ZIO
 import zio.s3.{MultipartUploadOptions, S3, multipartUpload}
 import zio.stream.ZStream
 
 import scala.reflect.ClassTag
-import purecsv.safe._
-import purecsv.safe.converter.RawFieldsConverter
-import purecsv.unsafe.RecordSplitter.defaultFieldSeparatorStr
-import purecsv.unsafe.converter.Converter
 
-protected[writers] class CsvWriter[ZE, T: ClassTag](sep: String = defaultFieldSeparatorStr)
+protected[writers] class CsvWriter[ZE, T: ClassTag](sep: String = ",")(implicit rfc: Converter[T,Seq[String]])
  extends S3Writer[ZE with S3 with S3Config, ZE, T, T] {
-implicit val c = new RawFieldsConverter
-  override def apply(stream: ZStream[ZE, Throwable, T]): ZIO[ZE with S3 with S3Config, Throwable, ZStream[ZE,
-    Throwable, T]] =
+  override def apply(stream: ZStream[ZE, Throwable, T]):
+  ZIO[ZE with S3 with S3Config, Throwable, ZStream[ZE, Throwable, T]] =
     for {
       config <- ZIO.service[S3Config]
       bucket <- config.taskS3Bucket
       location <- config.taskLocation
       bytesStream = stream
-        .map(s => new CSVRecord(s).toCSV() + "\n")
+        .map(a => rfc.to(a).mkString(sep) + "\n")
         .map(_.getBytes)
         .flatMap(bytes => ZStream.fromIterable(bytes))
       _ <- multipartUpload(
