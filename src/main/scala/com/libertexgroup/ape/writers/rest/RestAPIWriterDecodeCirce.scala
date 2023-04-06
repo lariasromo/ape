0efbc110d8e0fc9ab2200a2e24ba89da5a7da21a
Luis Arias <luisarias@encompasscorporation.com>
2023-04-06 16:38:57 +0100

    Rest writers Tests are broken

---
+++ src/main/scala/com/libertexgroup/ape/writers/rest/RestAPIWriterDecodeCirce.scala
@@ -0,0 +1,23 @@
+
+package com.libertexgroup.ape.writers.rest
+
+import com.libertexgroup.ape.utils.RestUtils.sendRequestString
+import com.libertexgroup.ape.utils.reLayer
+import io.circe.{Decoder, jawn}
+import zio.ZIO
+import zio.http.{Client, Request}
+import zio.stream.ZStream
+
+import scala.reflect.ClassTag
+
+protected[rest] class RestAPIWriterDecodeCirce[ZE, T: Decoder : ClassTag] extends RestApiWriter[Client, ZE, Request, T]
+ {
+  override def apply(i: ZStream[ZE, Throwable, Request]): ZIO[Client, Throwable, ZStream[ZE, Throwable, T]] =
+    for {
+      l <- reLayer[Client]
+      z = i.mapZIO(r => sendRequestString(r).provideSomeLayer(l))
+        .flatMap(x=>x).map(jawn.decode[T](_).toOption)
+        .filter(_.isDefined).map(_.get)
+    } yield z
+}
+
