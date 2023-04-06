efdb1f61fbeec571524930fb8ef2272fa94b0293
Luis Arias <luisarias@encompasscorporation.com>
2023-04-06 17:39:07 +0100

    Rest writers Tests are broken

---
+++ src/main/scala/com/libertexgroup/ape/writers/kafka/EncodeCirceWriter.scala
@@ -0,0 +1,33 @@
+package com.libertexgroup.ape.writers.kafka
+
+import com.libertexgroup.configs.KafkaConfig
+import io.circe.Encoder
+import io.circe.syntax.EncoderOps
+import org.apache.kafka.clients.producer.ProducerRecord
+import zio.kafka.producer.Producer
+import zio.kafka.serde.Serde
+import zio.stream.ZStream
+import zio.{ZIO, ZLayer}
+
+import scala.reflect.ClassTag
+
+protected[kafka] class EncodeCirceWriter[ET, Config <: KafkaConfig, T :ClassTag :Encoder]
+  extends KafkaWriter[Config, ET, String, T] {
+  override def apply(stream: ZStream[ET, Throwable, ProducerRecord[String, T]]):
+  ZIO[Config, Throwable, ZStream[ET, Throwable, ProducerRecord[String, T]]] =
+    for {
+      config <- ZIO.service[KafkaConfig]
+      s = stream.tap(v => {
+        ZIO.scoped {
+          Producer.produce[Any, String, String](
+            topic = config.topicName,
+            key = v.key(),
+            value = v.value().asJson.noSpaces,
+            keySerializer = Serde.string,
+            valueSerializer = Serde.string
+          ).provideSomeLayer(ZLayer.fromZIO(Producer.make(config.producerSettings)))
+        }
+      })
+
+    } yield s
+}
---
+++ src/main/scala/com/libertexgroup/ape/writers/kafka/EncodeWriter.scala
@@ -0,0 +1,31 @@
+package com.libertexgroup.ape.writers.kafka
+
+import com.libertexgroup.configs.KafkaConfig
+import org.apache.kafka.clients.producer.ProducerRecord
+import zio.kafka.producer.Producer
+import zio.kafka.serde.Serde
+import zio.stream.ZStream
+import zio.{ZIO, ZLayer}
+
+import scala.reflect.ClassTag
+
+protected[kafka] class EncodeWriter[ET, Config <: KafkaConfig, T :ClassTag](implicit val enc: T => String)
+  extends KafkaWriter[Config, ET, String, T] {
+  override def apply(stream: ZStream[ET, Throwable, ProducerRecord[String, T]]):
+  ZIO[Config, Throwable, ZStream[ET, Throwable, ProducerRecord[String, T]]] =
+    for {
+      config <- ZIO.service[KafkaConfig]
+      s = stream.tap(v => {
+        ZIO.scoped {
+          Producer.produce[Any, String, String](
+            topic = config.topicName,
+            key = v.key(),
+            value = enc(v.value()),
+            keySerializer = Serde.string,
+            valueSerializer = Serde.string
+          ).provideSomeLayer(ZLayer.fromZIO(Producer.make(config.producerSettings)))
+        }
+      })
+
+    } yield s
+}
