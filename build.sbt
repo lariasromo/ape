4c10b724de9c0019a162b01ddae971e9d275bba8
Luis Arias <luisarias@encompasscorporation.com>
2023-04-06 16:08:52 +0100

    Rest writers Tests are broken

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-ThisBuild / version := "2.1.0"
+ThisBuild / version := "2.1.1"
 
 
 ThisBuild / scalaVersion := "2.13.10"
--- src/main/scala/com/libertexgroup/ape/writers/PipelineWriters.scala
+++ src/main/scala/com/libertexgroup/ape/writers/PipelineWriters.scala
@@ -10,5 +10,6 @@ class PipelineWriters() {
   def jdbc[Config <: JDBCConfig :Tag] = new com.libertexgroup.ape.writers.jdbc.Writers[Config]()
   def kafka[Config <: KafkaConfig :Tag] = new com.libertexgroup.ape.writers.kafka.Writers[Config]()
   def misc = new com.libertexgroup.ape.writers.misc.Writers()
+  def rest = new com.libertexgroup.ape.writers.rest.Writers
   def s3[Config <: S3Config :Tag, AWSS3 <: S3 :Tag] = new com.libertexgroup.ape.writers.s3.Writers[Config, AWSS3]()
 }
