5342815602336a85f813a98a0fa1f5bdb7d241d7
Luis Arias <luisarias@encompasscorporation.com>
2023-04-24 16:33:43 +0100

    ALEX-1026 - Cassandra readers/writers with tests

--- src/main/scala/com/libertexgroup/ape/readers/cassandra/DefaultReader.scala
+++ src/main/scala/com/libertexgroup/ape/readers/cassandra/DefaultReader.scala
@@ -2,14 +2,16 @@ package com.libertexgroup.ape.readers.cassandra
 
 import com.datastax.oss.driver.api.core.cql.Row
 import com.libertexgroup.ape.utils.CassandraUtils.query2Chunk
+import com.libertexgroup.ape.utils.reLayer
 import com.libertexgroup.configs.CassandraConfig
 import com.libertexgroup.models.cassandra.CassandraModel
+import palanga.zio.cassandra.CassandraException
 import zio.stream.ZStream
 import zio.{Scope, ZIO}
 
 import scala.reflect.ClassTag
 
-protected[cassandra] class DefaultReader[E1, T <:CassandraModel :ClassTag , Config <: CassandraConfig]
-  (sql:String)(implicit t: Row => T) extends CassandraReader[Scope with Config, E1, T] {
-  override protected[this] def read: ZIO[Scope with Config, Throwable, ZStream[E1, Throwable, T]] = query2Chunk(sql)
+protected[cassandra] class DefaultReader[EZ, T <:CassandraModel :ClassTag , Config <: CassandraConfig]
+  (sql:String)(implicit t: Row => T) extends CassandraReader[Config, EZ, T] {
+  override protected[this] def read: ZIO[Config, Throwable, ZStream[EZ, Throwable, T]] = query2Chunk(sql)
 }
