5145713e7999d619bf4d84b873d5b23b541a2e8f
LuisArias <luis.arias@libertexgroup.com>
2023-06-12 14:56:29 +0100

    [skip-ci] Datahub test

--- connectors/datahub/src/test/scala/ape/datahub/RestEmitterTest.scala
+++ connectors/datahub/src/test/scala/ape/datahub/RestEmitterTest.scala
@@ -1,26 +1,33 @@
 package ape.datahub
 
 import ape.datahub.configs.DatahubConfig
-import ape.datahub.models.DummyModels._
+import ape.datahub.models.DummyModels.{Animal, Person, _}
 import ape.datahub.pipe.RestEmitterPipe
 import ape.pipe.Pipe
 import ape.reader.Reader
 import com.linkedin.common.FabricType
+import zio.Console.printLine
 import zio.stream.ZStream
 import zio.test.{Spec, TestEnvironment, ZIOSpec, assertTrue}
-import zio.{Scope, ZLayer}
+import zio.{Scope, ZIO, ZLayer}
 
 object RestEmitterTest extends ZIOSpec[DatahubConfig] {
+  val stream: ZStream[Any, Nothing, String] = ZStream("a", "b", "c")
 
-  val stream = ZStream("a", "b", "c")
+  val reader:       Reader[Any, Any, String] = Reader.UnitReader(stream)
+  val PersonPipe:   Pipe[Any, Any, String,   Person] = Pipe.UnitTPipe(n =>   Person(n))
+  val CarPipe:      Pipe[Any, Any, Person,   Car] = Pipe.UnitTPipe(p =>      Car(p.name))
+  val HousePipe:    Pipe[Any, Any, Car,      House] = Pipe.UnitTPipe(p =>    House(p.name))
+  val AirplanePipe: Pipe[Any, Any, House,    Airplane] = Pipe.UnitTPipe(p => Airplane(p.name))
+  val BuildingPipe: Pipe[Any, Any, Airplane, Building] = Pipe.UnitTPipe(p => Building(p.name))
+  val AnimalPipe:   Pipe[Any, Any, Building, Animal] = Pipe.UnitTPipe(p =>   Animal(p.name))
 
-  val reader = Reader.UnitReader(stream)
-  val pipe0 = RestEmitterPipe(Pipe.UnitTPipe[Any, Any, String,   Person](n =>   Person(n)))
-  val pipe1 = RestEmitterPipe(Pipe.UnitTPipe[Any, Any, Person,   Car](p =>      Car(p.name)))
-  val pipe2 = RestEmitterPipe(Pipe.UnitTPipe[Any, Any, Car,      House](p =>    House(p.name)))
-  val pipe3 = RestEmitterPipe(Pipe.UnitTPipe[Any, Any, House,    Airplane](p => Airplane(p.name)))
-  val pipe4 = RestEmitterPipe(Pipe.UnitTPipe[Any, Any, Airplane, Building](p => Building(p.name)))
-  val pipe5 = RestEmitterPipe(Pipe.UnitTPipe[Any, Any, Building, Animal](p =>   Animal(p.name)))
+  val pipe0 = RestEmitterPipe(PersonPipe)
+  val pipe1 = RestEmitterPipe(CarPipe)
+  val pipe2 = RestEmitterPipe(HousePipe)
+  val pipe3 = RestEmitterPipe(AirplanePipe)
+  val pipe4 = RestEmitterPipe(BuildingPipe)
+  val pipe5 = RestEmitterPipe(AnimalPipe)
 
   val pipeline = reader --> (pipe0 --> pipe1 --> pipe2 --> pipe3 --> pipe4 --> pipe5)
 
@@ -28,6 +35,7 @@ object RestEmitterTest extends ZIOSpec[DatahubConfig] {
     test("Emit pipeline"){
       for {
         data <- pipeline.runCollect
+        _ <- ZIO.foreach(data)(printLine(_))
       } yield {
         assertTrue(data.nonEmpty)
       }
@@ -36,9 +44,9 @@ object RestEmitterTest extends ZIOSpec[DatahubConfig] {
   override def bootstrap: ZLayer[Any, Any, DatahubConfig] = ZLayer.succeed(
     DatahubConfig(
       fabricType = FabricType.DEV,
-      tags = Seq("restTest", "ZIO", "happy", "alexandria")
-//      restEmitterUrl = "https://datahub.qa-env.com",
-//      restEmitterToken = "eyJhbGciOiJIUzI1NiJ9.eyJhY3RvclR5cGUiOiJVU0VSIiwiYWN0b3JJZCI6ImRhdGFodWJfZ3Vlc3QiLCJ0eXBlIjoiUEVSU09OQUwiLCJ2ZXJzaW9uIjoiMiIsImp0aSI6ImQzZjUzZTA0LTJhZjYtNGNkMi04MWJiLTUxMjAyYzc3MTBmYiIsInN1YiI6ImRhdGFodWJfZ3Vlc3QiLCJleHAiOjE2ODg5MjgyODEsImlzcyI6ImRhdGFodWItbWV0YWRhdGEtc2VydmljZSJ9.quUz-0I8UgNTypp_WhFbaMkLDBPx-dAhUCwy5MxDWM8"
+      tags = Seq("restTest", "ZIO", "happy", "alexandria"),
+      restEmitterUrl = "https://datahub-gms.qa-env.com:443",
+      restEmitterToken = ""
     )
   )
 }
