18a52d9052642ab664cabe1736c4342f901439a9
Luis Arias <luisarias@encompasscorporation.com>
2023-06-27 09:14:30 +0100

    Partition S3 by size and return file names

--- connectors/s3/src/main/scala/ape/s3/pipes/fromData/Pipes.scala
+++ connectors/s3/src/main/scala/ape/s3/pipes/fromData/Pipes.scala
@@ -5,14 +5,15 @@ import ape.s3.configs.S3Config
 import ape.utils.Utils.:=
 import com.sksamuel.avro4s.{Decoder, Encoder, SchemaFor}
 import zio.Tag
-import zio.s3.{S3, S3ObjectListing}
+import zio.s3.{S3, S3ObjectSummary}
 
 import scala.reflect.ClassTag
 
 protected[s3] class Pipes[Config <: S3Config :Tag]() {
   class encoded[ZE] {
     def avro[T >: Null : SchemaFor : Decoder : Encoder : ClassTag]:
-    Pipe[ZE with S3 with Config, ZE, T, S3ObjectListing] = new AvroPipe[ZE, T, Config]
+    Pipe[ZE with S3 with Config, ZE, T, S3ObjectSummary] = new AvroPipe[ZE, T, Config]
+//   todo: make parquet work with IAM roles
 //    def parquet[T >: Null : SchemaFor : Encoder : Decoder : ClassTag]
 //    (chunkSize: Int, duration: Duration): Pipe[ZE with S3 with Config, ZE, T, T] =
 //      new ParquetPipe[ZE, T, Config](chunkSize, duration)
@@ -20,19 +21,19 @@ protected[s3] class Pipes[Config <: S3Config :Tag]() {
   def encoded[ZE](implicit d1: ZE := Any) = new encoded[ZE]
 
   class text[ZE] {
-    def default: Pipe[ZE with S3 with Config, ZE, String, S3ObjectListing] = new TextPipe[ZE, Config]
+    def default: Pipe[ZE with S3 with Config, ZE, String, S3ObjectSummary] = new TextPipe[ZE, Config]
     def csv[T: ClassTag](
                           sep: String = ",",
                           order:Option[Seq[String]]=None
-                        ): Pipe[ZE with S3 with Config, ZE, T, S3ObjectListing] = new CsvPipe[ZE, T, Config](sep, order)
+                        ): Pipe[ZE with S3 with Config, ZE, T, S3ObjectSummary] = new CsvPipe[ZE, T, Config](sep, order)
   }
   def text[ZE](implicit d1: ZE := Any) = new text[ZE]
 
   class jsonLines[ZE] {
-    def default[T: ClassTag](implicit e: T => String): Pipe[ZE with S3 with Config, ZE, T, S3ObjectListing] =
+    def default[T: ClassTag](implicit e: T => String): Pipe[ZE with S3 with Config, ZE, T, S3ObjectSummary] =
       new JsonLinesPipe[ZE, T, Config]
 
-    def circe[T: io.circe.Encoder: ClassTag]: Pipe[ZE with S3 with Config, ZE, T, S3ObjectListing] =
+    def circe[T: io.circe.Encoder: ClassTag]: Pipe[ZE with S3 with Config, ZE, T, S3ObjectSummary] =
       new JsonLinesCircePipe[ZE, T, Config]
   }
   def jsonLines[ZE](implicit d1: ZE := Any) = new jsonLines[ZE]
