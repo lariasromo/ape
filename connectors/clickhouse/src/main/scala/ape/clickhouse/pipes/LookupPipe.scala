3c1afec751141b628c972d723f358ff9ea32af2f
Luis Arias <luisarias@encompasscorporation.com>
2023-07-19 16:20:46 +0100

    Clickhouse lookup model

--- connectors/clickhouse/src/main/scala/ape/clickhouse/pipes/LookupPipe.scala
+++ connectors/clickhouse/src/main/scala/ape/clickhouse/pipes/LookupPipe.scala
@@ -3,10 +3,9 @@ package ape.clickhouse.pipes
 import ape.clickhouse.configs.MultiClickhouseConfig
 import ape.clickhouse.models.ClickhouseLookupModel
 import ape.clickhouse.utils.ClickhouseJDBCUtils.query2ChunkMulti
-import ape.pipe.Pipe
 import ape.utils.Utils.reLayer
-import zio.{Chunk, Tag, ZIO}
 import zio.stream.ZStream
+import zio.{Chunk, Tag, ZIO}
 
 import java.sql.ResultSet
 import scala.reflect.ClassTag
@@ -16,17 +15,22 @@ protected[clickhouse] class LookupPipe[
   Config <: MultiClickhouseConfig :Tag,
   Model <: ClickhouseLookupModel[T] :Tag :ClassTag,
   T :ClassTag
-](implicit r: ResultSet => T) extends ClickhousePipe[Config, E, Model, (Model, Chunk[T])] {
+] extends ClickhousePipe[Config, E, Model, (Model, Chunk[T])] {
   override protected[this] def pipe(i: ZStream[E, Throwable, Model]):
     ZIO[Config, Throwable, ZStream[E, Throwable, (Model, Chunk[T])]] =
       for {
         rL <- reLayer[Config]
       } yield i.mapZIO(lookupModel => for {
-        results <- query2ChunkMulti[T](lookupModel.lookupQuery).provideSomeLayer(rL)
+        results <- {
+          implicit val decoder: ResultSet => T = lookupModel.lookupDecode
+          query2ChunkMulti[T](lookupModel.lookupQuery).provideSomeLayer(rL)
+        }
       } yield (lookupModel, results) )
 }
 
 object LookupPipe {
-  def lookup[Config <: MultiClickhouseConfig :Tag, E, Model <: ClickhouseLookupModel[T] :Tag :ClassTag, T :ClassTag]
-    (implicit r: ResultSet => T): LookupPipe[E, Config, Model, T] = new LookupPipe[E, Config, Model, T]
+  def lookup[ E,
+    Config <: MultiClickhouseConfig :Tag,
+    Model <: ClickhouseLookupModel[T] :Tag :ClassTag, T :ClassTag
+  ]: LookupPipe[E, Config, Model, T] = new LookupPipe[E, Config, Model, T]
 }
