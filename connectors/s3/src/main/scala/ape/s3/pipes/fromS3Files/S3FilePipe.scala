eb8d42c8fec242eab91b03d89dc44bd9a4738419
Luis Arias <luis.arias@libertexgroup.com>
2023-08-09 13:31:25 +0100

    CSV s3 pipes

--- connectors/s3/src/main/scala/ape/s3/pipes/fromS3Files/S3FilePipe.scala
+++ connectors/s3/src/main/scala/ape/s3/pipes/fromS3Files/S3FilePipe.scala
@@ -1,13 +1,16 @@
 package ape.s3.pipes.fromS3Files
 
-import ape.s3.configs.S3Config
+import ape.s3.configs.{CSVConfig, S3Config}
 import ape.s3.readers.{S3FileWithContent, readBytes, readPlainText}
 import ape.utils.Utils.reLayer
 import com.sksamuel.avro4s.{Decoder, Encoder, SchemaFor}
 import io.circe.jawn
+import purecsv.safe.CSVReader
 import zio.s3.S3ObjectSummary
 import zio.stream.ZStream
 import zio.{Tag, ZIO}
+
+import scala.reflect.ClassTag
 
 object S3FilePipe {
   def avroPipe[ZE, T: SchemaFor : Decoder : Encoder, S3Cfg <: S3Config : Tag](i: ZStream[ZE, Throwable, S3ObjectSummary]):
@@ -50,4 +53,17 @@ object S3FilePipe {
   } yield i.map(file =>
     (file, readPlainText(cfg.compressionType, file).provideSomeLayer(s3 ++ cfg.liveS3))
   )
+
+  def csvPipe[ZE, S3Cfg <: S3Config : Tag, T: ClassTag, CsvCfg <: CSVConfig](i: ZStream[ZE, Throwable, S3ObjectSummary]):
+    ZIO[S3Cfg with CsvCfg, Nothing, ZStream[ZE, Throwable, S3FileWithContent[T]]] = for {
+      csvCfg <- ZIO.service[CsvCfg]
+      cfg <- ZIO.service[S3Cfg]
+      s3 <- reLayer[S3Cfg]
+    } yield i.map(file => {
+      val stream = readPlainText(cfg.compressionType, file)
+        .map(l => CSVReader[T].readCSVFromString(l, csvCfg.delimiter, csvCfg.trimming, csvCfg.headers, csvCfg.headerMapping))
+        .map(a => a.map(_.toOption).filter(_.isDefined).map(_.get))
+        .flatMap(ZStream.fromIterable(_))
+      (file, stream.provideSomeLayer(s3 ++ cfg.liveS3))
+    })
 }
