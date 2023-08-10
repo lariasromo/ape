f7e5929e2f3641c9edba7c6002a2d822e6465cfb
Luis Arias <luis.arias@libertexgroup.com>
2023-08-10 17:44:41 +0100

    S3 pipes effect

--- connectors/s3/src/main/scala/ape/s3/pipes/fromData/Pipes.scala
+++ connectors/s3/src/main/scala/ape/s3/pipes/fromData/Pipes.scala
@@ -12,7 +12,7 @@ import scala.reflect.ClassTag
 protected[s3] class Pipes[Config <: S3Config :Tag]() {
   class encoded[ZE] {
     def avro[T >: Null : SchemaFor : Decoder : Encoder : ClassTag]:
-    Pipe[ZE with S3 with Config, ZE, T, S3ObjectSummary] = new AvroPipe[ZE, T, Config]
+    Pipe[ZE with Config, ZE, T, S3ObjectSummary] = new AvroPipe[ZE, T, Config]
 //   todo: make parquet work with IAM roles
 //    def parquet[T >: Null : SchemaFor : Encoder : Decoder : ClassTag]
 //    (chunkSize: Int, duration: Duration): Pipe[ZE with S3 with Config, ZE, T, T] =
@@ -21,7 +21,7 @@ protected[s3] class Pipes[Config <: S3Config :Tag]() {
   def encoded[ZE](implicit d1: ZE := Any) = new encoded[ZE]
 
   class text[ZE] {
-    def default: Pipe[ZE with S3 with Config, ZE, String, S3ObjectSummary] = new TextPipe[ZE, Config]
+    def default: Pipe[ZE with Config, ZE, String, S3ObjectSummary] = new TextPipe[ZE, Config]
     def csv[T: ClassTag](
                           sep: String = ",",
                           order:Option[Seq[String]]=None
@@ -30,10 +30,10 @@ protected[s3] class Pipes[Config <: S3Config :Tag]() {
   def text[ZE](implicit d1: ZE := Any) = new text[ZE]
 
   class jsonLines[ZE] {
-    def default[T: ClassTag](implicit e: T => String): Pipe[ZE with S3 with Config, ZE, T, S3ObjectSummary] =
+    def default[T: ClassTag](implicit e: T => String): Pipe[ZE with Config, ZE, T, S3ObjectSummary] =
       new JsonLinesPipe[ZE, T, Config]
 
-    def circe[T: io.circe.Encoder: ClassTag]: Pipe[ZE with S3 with Config, ZE, T, S3ObjectSummary] =
+    def circe[T: io.circe.Encoder: ClassTag]: Pipe[ZE with Config, ZE, T, S3ObjectSummary] =
       new JsonLinesCircePipe[ZE, T, Config]
   }
   def jsonLines[ZE](implicit d1: ZE := Any) = new jsonLines[ZE]
